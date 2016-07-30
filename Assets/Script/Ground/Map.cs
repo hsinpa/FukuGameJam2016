0fdbbcd30a39272584bc63b56faae5bf7c48765f
HsiN <hsinpa@gmail.com>
2016-07-30 17:18:12 +0800

    Merge remote-tracking branch 'origin/Jay'

--- Assets/Script/Ground/Map.cs
+++ Assets/Script/Ground/Map.cs
@@ -21,4 +21,5 @@ public class Map : MonoBehaviour {
 		});
 
 	}
+
 }
---
+++ Assets/Script/Player/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float Hp;
+    public float Mona;
+
+    public GameObject player;
+    public GameObject bullet;
+    public GameObject gun;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bullet, gun.transform.position, gun.transform.rotation);
+        }
+
+	}
+}
